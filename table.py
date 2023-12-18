3eaeb464f840e9cb996083a0345424b961f796ea
damn <146706005+slaVKiss@users.noreply.github.com>
2023-12-18 17:02:32 +0300

    Update table.py

--- table.py
+++ table.py
@@ -3,7 +3,7 @@ import csv
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QMessageBox
 
 
-# Load elements from the CSV file
+
 def load_elements(filename):
     with open(filename, encoding='utf-8') as f:
         reader = csv.reader(f)
@@ -11,7 +11,7 @@ def load_elements(filename):
         return {row[1]: row for row in reader}  # Create a dictionary with the symbol as a key
 
 
-# Main UI class for the Periodic Table
+
 class PeriodicTableUI(QMainWindow):
     def __init__(self, elements):
         super().__init__()
@@ -24,7 +24,7 @@ class PeriodicTableUI(QMainWindow):
         self.setCentralWidget(central_widget)
         layout = QVBoxLayout(central_widget)
 
-        # Create a button for each element and add it to the layout
+      
         for symbol in self.elements:
             btn = QPushButton(f'{self.elements[symbol][2]} ({symbol})', self)
             btn.clicked.connect(lambda ch, symbol=symbol: self.show_element_info(symbol))
@@ -39,14 +39,14 @@ class PeriodicTableUI(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    # Define your CSV headers - adjust according to your CSV file structure
+   
     headers = ['Atomic Number', 'Symbol', 'Element', 'Origin of name',
                'Group', 'Period', 'Atomic weight', 'Density',
                'Melting point', 'Boiling point',
                'Specific heat capacity', 'Electronegativity',
                'Abundance in earth\'s crust']
 
-    # Replace 'periodictable.csv' with the actual path to your CSV file
+  
     elements_dict = load_elements('periodictable.csv')
 
     # Create and show the main window
